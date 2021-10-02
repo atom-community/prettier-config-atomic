9ed6476afc83ccea79b577b880ca87998cb91a0a
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-10-02 14:34:09 -0500

    fix: inline the plugin into a function
    It seems prettier has a bug for resolving files

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,33 @@
+function prettier_plugin_packagejson() {
+  // Modified from https://github.com/matzkoh/prettier-plugin-packagejson to use the custom sort-package.json package
+  function requireSafe(path) {
+    try {
+      return require(path)
+    } catch (error) {}
+  }
+
+  const { parsers } = requireSafe("prettier/parser-babel") || requireSafe("prettier/parser-babylon")
+  const sortPackageJson = require("sort-package-json")
+  const parser = parsers["json-stringify"]
+
+  return {
+    parsers: {
+      "json-stringify": {
+        ...parser,
+        preprocess(text, options) {
+          if (parser.preprocess) {
+            text = parser.preprocess(text, options)
+          }
+
+          return options.filepath && /(^|\\|\/)package\.json$/.test(options.filepath) ? sortPackageJson(text) : text
+        },
+      },
+    },
+  }
+}
+
 module.exports = {
-  plugins: ["prettier-plugin-jsdoc", require("./prettier-plugin-packagejson.js")],
+  plugins: ["prettier-plugin-jsdoc", prettier_plugin_packagejson()],
   tabWidth: 2,
   printWidth: 120,
   semi: false,
